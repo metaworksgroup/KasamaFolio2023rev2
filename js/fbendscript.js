801d3a46ed51ed8df174d66e015884d69fd0dcb4
metaworks <metaworksgroup@gmail.com>
2023-04-03 11:56:52 +0700

    first commit

---
+++ js/fbendscript.js
@@ -0,0 +1,22 @@
+﻿function actionHtmlWindow(str) {
+ var a =	new ActionHtmlWindow(str); return a;
+}
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KPXKTBG');
+<!-- End Google Tag Manager -->
+
+	var googleAcountID="6080526119";
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', googleAcountID]);
+  _gaq.push(['_trackPageview']);
+
+  (function() { 
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+
